b25fcecbd51b62c1c69e0b842f59e3db2a78ed7c
Andre Menezes <djdedemenezes@gmail.com>
2022-12-05 13:11:16 -0300

    add comments on main files of the day

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,21 @@
 class PagesController < ApplicationController
 
-  # Action is just a method
+  # Action is just a method inside a controller
   # Different name because we don't call this methods
   # ROUTE Will execute them when receive the request
+
   def about
     @partners = ['FIFA', 'UEFA']
   end
 
+  # IMPORTANT
+  # Instance variables defined inside one action
+  # are NOT AVAILABLE in any other action
   def home
-
     @players = ['messi', 'mbappe', 'benzema', 'vini jr.']
 
     # raise #=> STOPS COLD EXECUTION (DEBUGGER)
+
     # params[:player] gives back what the user typed in the input
     if params[:player]
       @players = @players.select { |player| player == params[:player] }
--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # get '/', to: 'pages#home', as: 'home'
   root "pages#home"
-
-  # 1. VERB ( GET / POST / PATCH / DELETE )
-  # 2. PATH
+  
+  # A route is made of:
+  # 1. VERB              (GET / POST / PATCH / PUT / DELETE)
+  # 2. PATH              (/ or /hufflepuff or /bands)
   # 3. Which controller will handle this request
-  # 3.1 Controller name ALWAYS on PLURAL!!!
-  # 4. Which action inside this controller will get executed
-  # 4.1 Action name the SAME as your path
+  # 4. Which action this controller will execute
 
   # SYNTAX
-  # verb '/path', to: 'controller_name#action', as: :nickname(prefix)
+  # verb '/path', to: 'controller#action', as: :prefix_name
 
   get '/about', to: 'pages#about', as: 'about'
   # get '/home', to: 'pages#home', as: 'home'
